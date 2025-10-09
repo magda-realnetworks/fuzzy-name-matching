e0c144e2e78c6c20b1ce7d9576237b363f8107bc
magda <magda.realnetworks@gmail.com>
2025-10-09 20:51:48 +0200

    fix watchlist

--- uvicorn.run.sh
+++ uvicorn.run.sh
@@ -7,5 +7,13 @@ uvicorn app.main:app \
     --host 127.0.0.1 \
     --port 8000 \
     --reload \
-    --reload-dir . \
+    --reload-dir ./app \
+    --reload-dir ./data \
+    --reload-dir ./static \
+    --reload-dir ./templates \
+    --reload-include '*.py' \
+    --reload-include '*.csv' \
+    --reload-include '*.html' \
+    --reload-include '*.css' \
+    --reload-include '*.svg' \
     --log-level trace
